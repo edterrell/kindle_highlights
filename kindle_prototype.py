a06447d6fa39ba5d60a5c745f4b89fcbf0f2fa44
Ed Terrell <edterrell@comcast.net>
2025-06-10 18:48:20 -0600

    changed st.code to st write for main block

--- kindle_prototype.py
+++ kindle_prototype.py
@@ -330,7 +330,7 @@ if __name__ == "__main__":
     if 'title' in st.session_state and 'cleaned_highlight' in st.session_state:
         st.markdown("Random Highlight")
         st.subheader(f"{st.session_state.title}")
-        st.code(textwrap.fill(st.session_state.cleaned_highlight, width=50))
+        st.write(textwrap.fill(st.session_state.cleaned_highlight, width=45))
 
 
         action = st.radio(
